6ab96ea8fdaf08e17d6c4c66cbcd06439c45b415
foxdream <niekaixunn0515@hotmail.com>
2022-12-09 06:54:20 +0800

    assets

---
+++ assets/index.js
@@ -0,0 +1,75 @@
+import search from './Search.png';
+import tick from './tick.png';
+import cross from './cross.png';
+import logo from './logo.png';
+import logo01 from './logo01.png';
+import menu from './menu.png';
+import discord from './discord.png';
+import instagram from './instagram.png';
+import telegram from './telegram.png';
+import twitter from './twitter.png';
+import upload from './upload.png';
+import nftcollection1 from './nftcollection1.png';
+import nftcollection2 from './nftcollection2.png';
+import nftcollection3 from './nftcollection3.png';
+import nftcollection4 from './nftcollection4.png';
+import nftcollection5 from './nftcollection5.png';
+import nftcollection6 from './nftcollection6.png';
+import nftcollection7 from './nftcollection7.png';
+import nftcollection8 from './nftcollection8.png';
+import nftcollection9 from './nftcollection9.png';
+import nftcollection10 from './nftcollection10.png';
+import card from './card.png';
+import card1 from './card1.png';
+import card2 from './card2.png';
+import card3 from './card3.png';
+import card4 from './card4.png';
+import card5 from './card5.png';
+import card6 from './card6.png';
+import card7 from './card7.png';
+import card8 from './card8.png';
+import card9 from './card9.png';
+import card10 from './card10.png';
+import right from './right.png';
+import left from './left.png';
+import loader from './loader.gif';
+import arrow from './arrow.png';
+
+export default {
+  search,
+  tick,
+  cross,
+  logo,
+  menu,
+  discord,
+  instagram,
+  telegram,
+  twitter,
+  upload,
+  nftcollection1,
+  nftcollection2,
+  nftcollection3,
+  nftcollection4,
+  nftcollection5,
+  nftcollection6,
+  nftcollection7,
+  nftcollection8,
+  nftcollection9,
+  nftcollection10,
+  card,
+  card1,
+  card2,
+  card3,
+  card4,
+  card5,
+  card6,
+  card7,
+  card8,
+  card9,
+  card10,
+  right,
+  left,
+  loader,
+  arrow,
+  logo01,
+};
